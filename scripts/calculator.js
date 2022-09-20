699bd57c6c92b388d10e24ca6f9bd0f6957cd2d2
Dylan Hull <dbh133@hotmail.com>
2022-09-19 21:26:39 -0400

    base code working.  Modifying to add extra functionality.

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,6 +2,7 @@ const btns = document.querySelectorAll('button');
 const display = document.querySelector('.calcDisplay');
 let num1 = 0;
 let num2 = 0;
+
 function resetDisplay() {
 	display.textContent = '0';
 }
@@ -10,11 +11,11 @@ function add(x, y) {
 	return x + y;
 }
 
-function subtract(x, y) {
+function sub(x, y) {
 	return x - y;
 }
 
-function multiply(x, y) {
+function mult(x, y) {
 	return x * y;
 }
 
@@ -24,6 +25,7 @@ function divide(x, y) {
 
 function inputNumbers() {
 	let operator = '';
+	let previousOperator = '';
 
 	btns.forEach((btn) => {
 		btn.addEventListener('click', () => {
@@ -35,12 +37,6 @@ function inputNumbers() {
 				}
 			}
 
-			if (operator !== '') {
-				num1 += parseInt(display.textContent);
-			} else {
-				num2 += parseInt(display.textContent);
-			}
-
 			if (btn.id === 'delete') {
 				resetDisplay();
 				num1 = 0;
@@ -49,35 +45,41 @@ function inputNumbers() {
 
 			if (btn.id === 'add') {
 				operator = btn.id;
+				num1 += parseInt(display.textContent);
 				resetDisplay();
 			}
 
 			if (btn.id === 'sub') {
 				operator = btn.id;
+				num1 += parseInt(display.textContent);
 				resetDisplay();
 			}
 
 			if (btn.id === 'mult') {
 				operator = btn.id;
+				num1 += parseInt(display.textContent);
 				resetDisplay();
 			}
 
 			if (btn.id === 'divide') {
 				operator = btn.id;
+				num1 += parseInt(display.textContent);
 				resetDisplay();
 			}
 
 			if (btn.id === 'equals') {
+				num2 = parseInt(display.textContent);
+
 				if (operator === 'add') {
 					display.textContent = add(num1, num2);
 					num1 = 0;
 					num2 = 0;
 				} else if (operator === 'sub') {
-					display.textContent = subtract(num1, num2);
+					display.textContent = sub(num1, num2);
 					num1 = 0;
 					num2 = 0;
 				} else if (operator === 'mult') {
-					display.textContent = multiply(num1, num2);
+					display.textContent = mult(num1, num2);
 					num1 = 0;
 					num2 = 0;
 				} else {
